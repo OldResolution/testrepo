72c1197377f0f12941a2d9b5fe2637e5a130840f
OldResolution <140333635+OldResolution@users.noreply.github.com>
2024-10-11 02:50:55 +0530

    third commit

--- test.py
+++ test.py
@@ -3,7 +3,11 @@
 def firstFunction():
     print("Function in first module")
 
+def SecondFunction():
+    print("Function in second module")
+
 def main():
+    print("First module name is {}".format(__name__))
     print("First module name is {}".format(__name__))
 
 if __name__ == '__main__':
